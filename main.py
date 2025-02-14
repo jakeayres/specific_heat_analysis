597a561ae20d486b043ef4e389838f40d7d77a5d
jakeayres <jake_ayres@hotmail.co.uk>
2025-02-14 11:43:15 +0000

    clean front matter

--- main.py
+++ main.py
@@ -12,11 +12,6 @@ def main():
 
     st.title('Specific Heat Analysis')
     st.write('A set of utilities for running and analysing long relaxation specific heat measurements.')
-    st.write('... Add some sparse theory/method notes here ...')
-
-    st.write('Hello')
-
-    st.text_input('Test')
 
 
 if __name__ == "__main__":
